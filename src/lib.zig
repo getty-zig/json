0ec69c3d0ae17eb801994a276ab7683305c5a026
Jason Phan <jason@ket.so>
2021-07-24 11:06:24 -0500

    ser: Make usingnamespace public

--- src/lib.zig
+++ src/lib.zig
@@ -1,4 +1,4 @@
-usingnamespace @import("ser.zig");
+pub usingnamespace @import("ser.zig");
 
 pub const Formatter = @import("formatter.zig").Formatter;
 pub const CompactFormatter = @import("formatters/compact.zig").Formatter;
