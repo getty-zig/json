f74eed2c52e94c65eed2ea9b8f6e4e4c2db57137
Jason Phan <jason@ket.so>
2021-07-23 22:53:46 -0500

    formatter: Add PrettyFormatter

--- src/formatter.zig
+++ src/formatter.zig
@@ -207,6 +207,7 @@ pub fn CompactFormatter(comptime Writer: type) type {
                 try writer.writeAll(value);
             }
 
+            /// TODO: Figure out what to do on ascii control
             fn writeCharEscape(_: *Self, writer: Writer, value: CharEscape) Writer.Error!void {
                 const s = switch (value) {
                     .Quote => "\\\"",
@@ -266,6 +267,213 @@ pub fn CompactFormatter(comptime Writer: type) type {
             fn endObjectValue(self: *Self, writer: Writer) Writer.Error!void {
                 _ = self;
                 _ = writer;
+            }
+
+            fn writeRawFragment(self: *Self, writer: Writer, value: []const u8) Writer.Error!void {
+                _ = self;
+
+                try writer.writeAll(value);
+            }
+        };
+    };
+}
+
+pub fn PrettyFormatter(comptime Writer: type) type {
+    return struct {
+        current: usize,
+        has_value: bool,
+        indentation: []const u8,
+
+        const Self = @This();
+
+        /// Construct a pretty printer formatter that defaults to using two
+        /// spaces for indentation.
+        pub fn init() Self {
+            return initWithIndent("  ");
+        }
+
+        /// Construct a pretty printer formatter that uses the `indent` string
+        /// for indentation.
+        pub fn initWithIndent(indentation: []const u8) Self {
+            return .{
+                .current = 0,
+                .has_value = false,
+                .indentation = indentation,
+            };
+        }
+
+        fn indent(self: *Self, writer: anytype) Writer.Error!void {
+            var i: usize = 0;
+
+            while (i < self.current) : (i += 1) {
+                try writer.writeAll(self.indentation);
+            }
+        }
+
+        pub const F = Formatter(
+            *Self,
+            Writer,
+            _F.writeNull,
+            _F.writeBool,
+            _F.writeInt,
+            _F.writeFloat,
+            _F.writeNumberString,
+            _F.beginString,
+            _F.endString,
+            _F.writeStringFragment,
+            _F.writeCharEscape,
+            _F.beginArray,
+            _F.endArray,
+            _F.beginArrayValue,
+            _F.endArrayValue,
+            _F.beginObject,
+            _F.endObject,
+            _F.beginObjectKey,
+            _F.endObjectKey,
+            _F.beginObjectValue,
+            _F.endObjectValue,
+            _F.writeRawFragment,
+        );
+
+        pub fn formatter(self: *Self) F {
+            return .{ .context = self };
+        }
+
+        const _F = struct {
+            fn writeNull(_: *Self, writer: Writer) Writer.Error!void {
+                try writer.writeAll("null");
+            }
+
+            fn writeBool(_: *Self, writer: Writer, value: bool) Writer.Error!void {
+                try writer.writeAll(if (value) "true" else "false");
+            }
+
+            fn writeInt(_: *Self, writer: Writer, value: anytype) Writer.Error!void {
+                var buf: [100]u8 = undefined;
+                try writer.writeAll(std.fmt.bufPrintIntToSlice(&buf, value, 10, .lower, .{}));
+            }
+
+            fn writeFloat(_: *Self, writer: Writer, value: anytype) Writer.Error!void {
+                var buf: [512]u8 = undefined;
+                var stream = std.io.fixedBufferStream(&buf);
+
+                std.fmt.formatFloatDecimal(value, std.fmt.FormatOptions{}, stream.writer()) catch |err| switch (err) {
+                    error.NoSpaceLeft => unreachable,
+                    else => unreachable, // TODO: handle error
+                };
+
+                // TODO: fix getPos error
+                try writer.writeAll(buf[0 .. stream.getPos() catch unreachable]);
+            }
+
+            fn writeNumberString(_: *Self, writer: Writer, value: []const u8) Writer.Error!void {
+                try writer.writeAll(value);
+            }
+
+            fn beginString(_: *Self, writer: Writer) Writer.Error!void {
+                try writer.writeAll("\"");
+            }
+
+            fn endString(_: *Self, writer: Writer) Writer.Error!void {
+                try writer.writeAll("\"");
+            }
+
+            fn writeStringFragment(_: *Self, writer: Writer, value: []const u8) Writer.Error!void {
+                try writer.writeAll(value);
+            }
+
+            /// TODO: Figure out what to do on ascii control
+            fn writeCharEscape(_: *Self, writer: Writer, value: CharEscape) Writer.Error!void {
+                const s = switch (value) {
+                    .Quote => "\\\"",
+                    .ReverseSolidus => "\\\\",
+                    .Solidus => "\\/",
+                    .Backspace => "\\",
+                    .FormFeed => "\\f",
+                    .LineFeed => "\\n",
+                    .CarriageReturn => "\\r",
+                    .Tab => "\\t",
+                    //.AsciiControl
+                };
+
+                try writer.writeAll(s);
+            }
+
+            fn beginArray(self: *Self, writer: Writer) Writer.Error!void {
+                self.current += 1;
+                self.has_value = false;
+                try writer.writeAll("[");
+            }
+
+            fn endArray(self: *Self, writer: Writer) Writer.Error!void {
+                self.current -= 1;
+
+                if (self.has_value) {
+                    try writer.writeAll("\n");
+                    try self.indent(writer);
+                }
+
+                try writer.writeAll("]");
+            }
+
+            fn beginArrayValue(self: *Self, writer: Writer, first: bool) Writer.Error!void {
+                if (first) {
+                    try writer.writeAll("\n");
+                } else {
+                    try writer.writeAll(",\n");
+                }
+
+                try self.indent(writer);
+            }
+
+            fn endArrayValue(self: *Self, writer: Writer) Writer.Error!void {
+                _ = writer;
+
+                self.has_value = true;
+            }
+
+            fn beginObject(self: *Self, writer: Writer) Writer.Error!void {
+                self.current += 1;
+                self.has_value = false;
+                try writer.writeAll("{");
+            }
+
+            fn endObject(self: *Self, writer: Writer) Writer.Error!void {
+                self.current -= 1;
+
+                if (self.has_value) {
+                    try writer.writeAll("\n");
+                    try self.indent(writer);
+                }
+
+                try writer.writeAll("}");
+            }
+
+            fn beginObjectKey(self: *Self, writer: Writer, first: bool) Writer.Error!void {
+                if (first) {
+                    try writer.writeAll("\n");
+                } else {
+                    try writer.writeAll(",\n");
+                }
+
+                try self.indent(writer);
+            }
+
+            fn endObjectKey(self: *Self, writer: Writer) Writer.Error!void {
+                _ = self;
+                _ = writer;
+            }
+
+            fn beginObjectValue(self: *Self, writer: Writer) Writer.Error!void {
+                _ = self;
+
+                try writer.writeAll(": ");
+            }
+
+            fn endObjectValue(self: *Self, writer: Writer) Writer.Error!void {
+                _ = writer;
+
+                self.has_value = true;
             }
 
             fn writeRawFragment(self: *Self, writer: Writer, value: []const u8) Writer.Error!void {
@@ -361,7 +569,7 @@ test "formatter" {
     var stdout = std.io.getStdOut();
     const writer = stdout.writer();
 
-    var compact_formatter = CompactFormatter(@TypeOf(writer)){};
+    var compact_formatter = PrettyFormatter(@TypeOf(writer)).init();
     const formatter = compact_formatter.formatter();
 
     try formatter.writeNumberString(writer, "\n");
@@ -395,9 +603,14 @@ test "formatter" {
     try formatter.writeNumberString(writer, "\n");
     try formatter.writeNumberString(writer, "\n");
 
-    try formatter.beginString(writer);
+    try formatter.beginArray(writer);
+    try formatter.beginArrayValue(writer, true);
     try formatter.writeBool(writer, true);
-    try formatter.endString(writer);
+    try formatter.endArrayValue(writer);
+    try formatter.beginArrayValue(writer, false);
+    try formatter.writeBool(writer, false);
+    try formatter.endArrayValue(writer);
+    try formatter.endArray(writer);
 
     try formatter.writeNumberString(writer, "\n");
     try formatter.writeNumberString(writer, "\n");
