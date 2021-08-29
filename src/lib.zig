442738c3b059bec39b16bfb0f93f43fb0aefef78
Jason Phan <jason@ket.so>
2021-08-28 20:42:10 -0500

    Rename ser.zig to serializer.zig

--- src/lib.zig
+++ src/lib.zig
@@ -1,4 +1,4 @@
-pub usingnamespace @import("ser/ser.zig");
+pub usingnamespace @import("ser/serializer.zig");
 pub usingnamespace @import("ser/fmt/escape.zig");
 pub usingnamespace @import("ser/fmt/formatter.zig");
 pub usingnamespace @import("ser/fmt/formatters/compact.zig");
