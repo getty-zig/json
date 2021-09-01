1aa70b35126dc991135d2cc3471c907f799c877a
Jason Phan <jason@ket.so>
2021-09-01 09:50:58 -0500

    Remove usingnamespace (#5)
    Closes #5.

--- src/lib.zig
+++ src/lib.zig
@@ -1,5 +1,25 @@
-pub usingnamespace @import("ser/serializer.zig");
-pub usingnamespace @import("ser/fmt/escape.zig");
-pub usingnamespace @import("ser/fmt/formatter.zig");
-pub usingnamespace @import("ser/fmt/formatters/compact.zig");
-pub usingnamespace @import("ser/fmt/formatters/pretty.zig");
+const serializer = @import("ser/serializer.zig");
+const escape = @import("ser/fmt/escape.zig");
+
+pub const Serializer = serializer.Serializer;
+pub const toWriter = serializer.toWriter;
+pub const toPrettyWriter = serializer.toPrettyWriter;
+pub const toWriterWith = serializer.toWriterWith;
+pub const toPrettyWriterWith = serializer.toPrettyWriterWith;
+pub const toString = serializer.toString;
+pub const toPrettyString = serializer.toPrettyString;
+pub const toStringWith = serializer.toStringWith;
+pub const toPrettyStringWith = serializer.toPrettyStringWith;
+
+pub const CharEscape = escape.CharEscape;
+pub const formatEscapedString = escape.formatEscapedString;
+
+pub const Formatter = @import("ser/fmt/formatter.zig").Formatter;
+pub const CompactFormatter = @import("ser/fmt/formatters/compact.zig").CompactFormatter;
+pub const PrettyFormatter = @import("ser/fmt/formatters/pretty.zig").PrettyFormatter;
+
+test {
+    refAllDecls(@This());
+}
+
+const refAllDecls = @import("std").testing.refAllDecls;
