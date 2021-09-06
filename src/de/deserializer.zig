9bc416d569aba2cc0f7000bb0ebb6e33d8a2c404
Jason Phan <jason@ket.so>
2021-09-06 13:43:35 -0500

    Remove unneeded namescope in Deserializer

--- src/de/deserializer.zig
+++ src/de/deserializer.zig
@@ -27,128 +27,126 @@ pub fn Deserializer(comptime Reader: type) type {
         /// Implements `getty.de.Deserializer`.
         pub usingnamespace getty.de.Deserializer(
             *Self,
-            _D.Error,
-            _D.deserializeBool,
+            Error,
+            deserializeBool,
             undefined,
-            //_D.deserializeEnum,
-            _D.deserializeFloat,
-            _D.deserializeInt,
+            //deserializeEnum,
+            deserializeFloat,
+            deserializeInt,
             undefined,
-            //_D.deserializeMap,
-            _D.deserializeOptional,
-            _D.deserializeSequence,
+            //deserializeMap,
+            deserializeOptional,
+            deserializeSequence,
             undefined,
-            //_D.deserializeString,
+            //deserializeString,
             undefined,
-            //_D.deserializeStruct,
-            _D.deserializeVoid,
+            //deserializeStruct,
+            deserializeVoid,
         );
 
-        const _D = struct {
-            const Error = error{Input};
+        const Error = error{Input};
 
-            fn deserializeBool(self: *Self, visitor: anytype) !@TypeOf(visitor).Value {
-                if (self.tokens.next() catch return Error.Input) |token| {
-                    switch (token) {
-                        .True => return try visitor.visitBool(Error, true),
-                        .False => return try visitor.visitBool(Error, false),
-                        else => {},
-                    }
+        fn deserializeBool(self: *Self, visitor: anytype) !@TypeOf(visitor).Value {
+            if (self.tokens.next() catch return Error.Input) |token| {
+                switch (token) {
+                    .True => return try visitor.visitBool(Error, true),
+                    .False => return try visitor.visitBool(Error, false),
+                    else => {},
                 }
-
-                return Error.Input;
             }
 
-            fn deserializeFloat(self: *Self, visitor: anytype) !@TypeOf(visitor).Value {
-                if (self.tokens.next() catch return Error.Input) |token| {
-                    switch (token) {
-                        .Number => |num| return try visitor.visitFloat(
-                            Error,
-                            std.fmt.parseFloat(@TypeOf(visitor).Value, num.slice(self.buffer.items, self.tokens.i - 1)) catch return Error.Input,
-                        ),
-                        else => {},
-                    }
-                }
+            return Error.Input;
+        }
 
-                return Error.Input;
-            }
-
-            fn deserializeInt(self: *Self, visitor: anytype) !@TypeOf(visitor).Value {
-                const Value = @TypeOf(visitor).Value;
-
-                if (self.tokens.next() catch return Error.Input) |token| {
-                    switch (token) {
-                        .Number => |num| switch (num.is_integer) {
-                            true => return try visitor.visitInt(
-                                Error,
-                                std.fmt.parseInt(Value, num.slice(self.buffer.items, self.tokens.i - 1), 10) catch return Error.Input,
-                            ),
-                            false => return visitor.visitFloat(
-                                Error,
-                                std.fmt.parseFloat(f128, num.slice(self.buffer.items, self.tokens.i - 1)) catch return Error.Input,
-                            ),
-                        },
-                        else => {},
-                    }
-                }
-
-                return Error.Input;
-            }
-
-            fn deserializeSequence(self: *Self, visitor: anytype) !@TypeOf(visitor).Value {
-                var access = struct {
-                    d: @typeInfo(@TypeOf(Self.deserializer)).Fn.return_type.?,
-
-                    pub usingnamespace getty.de.SequenceAccess(
-                        *@This(),
+        fn deserializeFloat(self: *Self, visitor: anytype) !@TypeOf(visitor).Value {
+            if (self.tokens.next() catch return Error.Input) |token| {
+                switch (token) {
+                    .Number => |num| return try visitor.visitFloat(
                         Error,
-                        nextElementSeed,
-                    );
-
-                    fn nextElementSeed(a: *@This(), seed: anytype) !?@TypeOf(seed).Value {
-                        return try seed.deserialize(a.d);
-                    }
-                }{ .d = self.deserializer() };
-
-                if (self.tokens.next() catch return Error.Input) |token| {
-                    if (token == .ArrayBegin) {
-                        return try visitor.visitSequence(access.sequenceAccess());
-                    }
+                        std.fmt.parseFloat(@TypeOf(visitor).Value, num.slice(self.buffer.items, self.tokens.i - 1)) catch return Error.Input,
+                    ),
+                    else => {},
                 }
-
-                return Error.Input;
             }
 
-            fn deserializeOptional(self: *Self, visitor: anytype) !@TypeOf(visitor).Value {
-                const tokens = self.tokens;
+            return Error.Input;
+        }
 
-                if (self.tokens.next() catch return Error.Input) |token| {
-                    return try switch (token) {
-                        .Null => visitor.visitNull(Error),
-                        else => blk: {
-                            // Get back the token we just ate if it was an
-                            // actual value so that whenever the next
-                            // deserialize method is called by visitSome,
-                            // they'll eat the token we just saw instead of
-                            // whatever is after it.
-                            self.tokens = tokens;
-                            break :blk visitor.visitSome(self.deserializer());
-                        },
-                    };
+        fn deserializeInt(self: *Self, visitor: anytype) !@TypeOf(visitor).Value {
+            const Value = @TypeOf(visitor).Value;
+
+            if (self.tokens.next() catch return Error.Input) |token| {
+                switch (token) {
+                    .Number => |num| switch (num.is_integer) {
+                        true => return try visitor.visitInt(
+                            Error,
+                            std.fmt.parseInt(Value, num.slice(self.buffer.items, self.tokens.i - 1), 10) catch return Error.Input,
+                        ),
+                        false => return visitor.visitFloat(
+                            Error,
+                            std.fmt.parseFloat(f128, num.slice(self.buffer.items, self.tokens.i - 1)) catch return Error.Input,
+                        ),
+                    },
+                    else => {},
                 }
-
-                return Error.Input;
             }
 
-            fn deserializeVoid(self: *Self, visitor: anytype) !@TypeOf(visitor).Value {
-                if (self.tokens.next() catch return Error.Input) |token| {
-                    if (token == .Null) {
-                        return try visitor.visitVoid(Error);
-                    }
-                }
+            return Error.Input;
+        }
 
-                return Error.Input;
+        fn deserializeSequence(self: *Self, visitor: anytype) !@TypeOf(visitor).Value {
+            var access = struct {
+                d: @typeInfo(@TypeOf(Self.deserializer)).Fn.return_type.?,
+
+                pub usingnamespace getty.de.SequenceAccess(
+                    *@This(),
+                    Error,
+                    nextElementSeed,
+                );
+
+                fn nextElementSeed(a: *@This(), seed: anytype) !?@TypeOf(seed).Value {
+                    return try seed.deserialize(a.d);
+                }
+            }{ .d = self.deserializer() };
+
+            if (self.tokens.next() catch return Error.Input) |token| {
+                if (token == .ArrayBegin) {
+                    return try visitor.visitSequence(access.sequenceAccess());
+                }
             }
-        };
+
+            return Error.Input;
+        }
+
+        fn deserializeOptional(self: *Self, visitor: anytype) !@TypeOf(visitor).Value {
+            const tokens = self.tokens;
+
+            if (self.tokens.next() catch return Error.Input) |token| {
+                return try switch (token) {
+                    .Null => visitor.visitNull(Error),
+                    else => blk: {
+                        // Get back the token we just ate if it was an
+                        // actual value so that whenever the next
+                        // deserialize method is called by visitSome,
+                        // they'll eat the token we just saw instead of
+                        // whatever is after it.
+                        self.tokens = tokens;
+                        break :blk visitor.visitSome(self.deserializer());
+                    },
+                };
+            }
+
+            return Error.Input;
+        }
+
+        fn deserializeVoid(self: *Self, visitor: anytype) !@TypeOf(visitor).Value {
+            if (self.tokens.next() catch return Error.Input) |token| {
+                if (token == .Null) {
+                    return try visitor.visitVoid(Error);
+                }
+            }
+
+            return Error.Input;
+        }
     };
 }
