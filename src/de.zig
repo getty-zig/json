5b7f1efc517896c7977cba00025bbbbec5db7987
Jason Phan <jason@ket.so>
2021-09-12 21:01:12 -0500

    de: Add convenience testing imports

--- src/de.zig
+++ src/de.zig
@@ -1,6 +1,12 @@
 const getty = @import("getty");
 const std = @import("std");
 
+const eql = std.mem.eql;
+const testing = std.testing;
+
+const expect = testing.expect;
+const expectEqual = testing.expectEqual;
+
 pub const de = struct {
     pub usingnamespace @import("de/deserializer.zig");
 };
@@ -18,64 +24,64 @@ pub fn fromString(allocator: *std.mem.Allocator, comptime T: type, string: []con
 }
 
 test "array" {
-    try std.testing.expectEqual([2]bool{ false, false }, try fromString(std.testing.allocator, [2]bool, "[false,false]"));
-    try std.testing.expectEqual([2]bool{ true, true }, try fromString(std.testing.allocator, [2]bool, "[true,true]"));
-    try std.testing.expectEqual([2]bool{ true, false }, try fromString(std.testing.allocator, [2]bool, "[true,false]"));
-    try std.testing.expectEqual([2]bool{ false, true }, try fromString(std.testing.allocator, [2]bool, "[false,true]"));
+    try expectEqual([2]bool{ false, false }, try fromString(testing.allocator, [2]bool, "[false,false]"));
+    try expectEqual([2]bool{ true, true }, try fromString(testing.allocator, [2]bool, "[true,true]"));
+    try expectEqual([2]bool{ true, false }, try fromString(testing.allocator, [2]bool, "[true,false]"));
+    try expectEqual([2]bool{ false, true }, try fromString(testing.allocator, [2]bool, "[false,true]"));
 
-    try std.testing.expectEqual([5]i32{ 1, 2, 3, 4, 5 }, try fromString(std.testing.allocator, [5]i32, "[1,2,3,4,5]"));
+    try expectEqual([5]i32{ 1, 2, 3, 4, 5 }, try fromString(testing.allocator, [5]i32, "[1,2,3,4,5]"));
 
-    try std.testing.expectEqual([2][1]i32{ .{1}, .{2} }, try fromString(std.testing.allocator, [2][1]i32, "[[1],[2]]"));
-    try std.testing.expectEqual([2][1][3]i32{ .{.{ 1, 2, 3 }}, .{.{ 4, 5, 6 }} }, try fromString(std.testing.allocator, [2][1][3]i32, "[[[1,2,3]],[[4,5,6]]]"));
+    try expectEqual([2][1]i32{ .{1}, .{2} }, try fromString(testing.allocator, [2][1]i32, "[[1],[2]]"));
+    try expectEqual([2][1][3]i32{ .{.{ 1, 2, 3 }}, .{.{ 4, 5, 6 }} }, try fromString(testing.allocator, [2][1][3]i32, "[[[1,2,3]],[[4,5,6]]]"));
 }
 
 test "bool" {
-    try std.testing.expectEqual(true, try fromString(std.testing.allocator, bool, "true"));
-    try std.testing.expectEqual(false, try fromString(std.testing.allocator, bool, "false"));
+    try expectEqual(true, try fromString(testing.allocator, bool, "true"));
+    try expectEqual(false, try fromString(testing.allocator, bool, "false"));
 }
 
 test "int" {
-    try std.testing.expectEqual(@as(u32, 1), try fromString(std.testing.allocator, u32, "1"));
-    try std.testing.expectEqual(@as(i32, -1), try fromString(std.testing.allocator, i32, "-1"));
-    try std.testing.expectEqual(@as(u32, 1), try fromString(std.testing.allocator, u32, "1.0"));
-    try std.testing.expectEqual(@as(i32, -1), try fromString(std.testing.allocator, i32, "-1.0"));
+    try expectEqual(@as(u32, 1), try fromString(testing.allocator, u32, "1"));
+    try expectEqual(@as(i32, -1), try fromString(testing.allocator, i32, "-1"));
+    try expectEqual(@as(u32, 1), try fromString(testing.allocator, u32, "1.0"));
+    try expectEqual(@as(i32, -1), try fromString(testing.allocator, i32, "-1.0"));
 }
 
 test "float" {
-    try std.testing.expectEqual(@as(f32, 3.14), try fromString(std.testing.allocator, f32, "3.14"));
-    try std.testing.expectEqual(@as(f64, 3.14), try fromString(std.testing.allocator, f64, "3.14"));
-    try std.testing.expectEqual(@as(f32, 3.0), try fromString(std.testing.allocator, f32, "3"));
-    try std.testing.expectEqual(@as(f64, 3.0), try fromString(std.testing.allocator, f64, "3"));
+    try expectEqual(@as(f32, 3.14), try fromString(testing.allocator, f32, "3.14"));
+    try expectEqual(@as(f64, 3.14), try fromString(testing.allocator, f64, "3.14"));
+    try expectEqual(@as(f32, 3.0), try fromString(testing.allocator, f32, "3"));
+    try expectEqual(@as(f64, 3.0), try fromString(testing.allocator, f64, "3"));
 }
 
-test "slice" {
-    const string = try fromString(std.testing.allocator, []const u8, "\"Hello, World!\"");
-    defer std.testing.allocator.free(string);
+test "slice (string)" {
+    const string = try fromString(testing.allocator, []const u8, "\"Hello, World!\"");
+    defer testing.allocator.free(string);
 
-    try std.testing.expect(std.mem.eql(u8, "Hello, World!", string));
+    try expect(eql(u8, "Hello, World!", string));
 }
 
 test "struct" {
-    const got = try fromString(std.testing.allocator, struct { x: i32, y: []const u8 },
+    const got = try fromString(testing.allocator, struct { x: i32, y: []const u8 },
         \\{"x":1,"y":"Hello"}
     );
-    defer std.testing.allocator.free(got.y);
+    defer testing.allocator.free(got.y);
 
-    try std.testing.expectEqual(@as(i32, 1), got.x);
-    try std.testing.expect(std.mem.eql(u8, "Hello", got.y));
+    try expectEqual(@as(i32, 1), got.x);
+    try expect(eql(u8, "Hello", got.y));
 }
 
 test "optional" {
-    try std.testing.expectEqual(@as(?i32, null), try fromString(std.testing.allocator, ?i32, "null"));
-    try std.testing.expectEqual(@as(?i32, 42), try fromString(std.testing.allocator, ?i32, "42"));
+    try expectEqual(@as(?i32, null), try fromString(testing.allocator, ?i32, "null"));
+    try expectEqual(@as(?i32, 42), try fromString(testing.allocator, ?i32, "42"));
 }
 
 test "void" {
-    try std.testing.expectEqual({}, try fromString(std.testing.allocator, void, "null"));
-    try std.testing.expectError(error.Input, fromString(std.testing.allocator, void, "true"));
-    try std.testing.expectError(error.Input, fromString(std.testing.allocator, void, "1"));
+    try expectEqual({}, try fromString(testing.allocator, void, "null"));
+    try testing.expectError(error.Input, fromString(testing.allocator, void, "true"));
+    try testing.expectError(error.Input, fromString(testing.allocator, void, "1"));
 }
 
 test {
-    std.testing.refAllDecls(@This());
+    testing.refAllDecls(@This());
 }
