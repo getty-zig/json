b5f00e03e73c7f3efe9b1b3feb9ddce2ce60a64f
Jason Phan <jason@ket.so>
2021-09-21 13:53:48 -0500

    ser: Update documentation for escape.zig

--- src/ser/escape.zig
+++ src/ser/escape.zig
@@ -1,3 +1,37 @@
+//! JSON escaping-related code.
+//!
+//! The escaping rules used in this module originate from Fastly:
+//! https://developer.fastly.com/reference/vcl/functions/strings/json-escape/.
+//!
+//! The rules are as follows (in priority order):
+//!
+//!   1. If the code point is the double quote (0x22), it is escaped as \".
+//!
+//!   2. If the code point is the backslash (0x5C), it is escaped as \\.
+//!
+//!   3. If the code point is listed below, they are escaped accordingly:
+//!
+//!       * 0x08 (backspace)        ->  \b
+//!       * 0x09 (horizontal tab)   ->  \t
+//!       * 0x0A (newline)          ->  \n
+//!       * 0x0C (form feed)        ->  \f
+//!       * 0x0D (carriage return)  ->  \r
+//!
+//!   4. If the code point is less than or equal to 0x1F, or is equal to
+//!   0x7F, 0x2028, or 0x2029, then it is a control character that wasn't
+//!   listed above, and is escaped as \uHHHH where 'HHHH' is the hexadecimal
+//!   value of the code point.
+//!
+//!   5. If the code point is greater than 0xFFFF (i.e., it is beyond the Basic
+//!   Multilingual Plane of Unicode), the code point is converted into a UTF-16
+//!   surrogate pair with the \\u notation (e.g., U+1F601, or '😁', would be
+//!   escaped as \uD83D\uDE01).
+//!
+//!   6. If none of the preceding rules match and there is a sequence of
+//!   valid UTF-8 bytes, the bytes are passed through as-is (e.g., the code
+//!   point U+0061 would be passed through as 'a').
+//!
+//!   7. If there is a byte sequence of invalid UTF-8, the conversion fails.
 const std = @import("std");
 
 pub const DOUBLE_QUOTE = '\"';
@@ -8,40 +42,12 @@ pub const NEWLINE = '\n';
 pub const FORM_FEED = 0x0C;
 pub const CARRIAGE_RETURN = '\r';
 
-pub const HEX_DIGITS = [_]u8{ '0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'a', 'b', 'c', 'd', 'e', 'f' };
+pub const HEX_DIGITS = [_]u8{
+    '0', '1', '2', '3', '4', '5', '6', '7', '8', '9',
+    'a', 'b', 'c', 'd', 'e', 'f',
+};
 
-/// From Fastly, the JSON escape rules are as follows (in priority order):
-///
-///     1. If the code point is the double quote (0x22), it is escaped as \".
-///
-///     2. If the code point is the backslash (0x5C), it is escaped as \\.
-///
-///     3. The following code points are escaped as follows:
-///
-///         * 0x08 (backspace)       -> \b
-///         * 0x09 (horizontal tab)  -> \t
-///         * 0x0A (newline)         -> \n
-///         * 0x0C (form feed)       -> \f
-///         * 0x0D (carriage return) -> \r
-///
-///     4. If the code point is less than or equal to 0x1F, or is equal to
-///     0x7F, 0x2028, or 0x2029, then it is a control character that wasn't
-///     listed above, and is escaped as \uHHHH where 'HHHH' is the hexadecimal
-///     value of the code point.
-///
-///     5. If the code point is greater than 0xFFFF (i.e., beyond the Basic
-///     Multilingual Plane of Unicode), the code point is converted into a
-///     UTF-16 surrogate pair with the \\u notation (e.g., U+1F601, or '😁',
-///     would be escaped as \uD83D\uDE01).
-///
-///     6. If none of the preceding rules match and there is a sequence of
-///     valid UTF-8 bytes, the bytes are passed through as-is (e.g., the code
-///     point U+0061 would be passed through as 'a').
-///
-///     7. If there is a byte sequence of invalid UTF-8, the conversion fails.
-///
-/// Note that the rules imply that no code points between 0x7F and 0x10000 are
-/// escaped as \\uHHHH except for the code poitns U+2028 and U+2029.
+/// Escapes characters of a UTF-8 encoded string using JSON escape sequences.
 pub fn escape(bytes: []const u8, writer: anytype, formatter: anytype) !void {
     var i: usize = 0;
     var start: usize = 0;
@@ -51,7 +57,7 @@ pub fn escape(bytes: []const u8, writer: anytype, formatter: anytype) !void {
         const length = std.unicode.utf8ByteSequenceLength(byte) catch unreachable;
         const codepoint = std.unicode.utf8Decode(bytes[i .. i + length]) catch unreachable;
 
-        // Skip byte if it doesn't need escaping.
+        // Skip code point if it doesn't require escaping.
         switch (byte) {
             DOUBLE_QUOTE, BACKSLASH, BACKSPACE, TAB, NEWLINE, FORM_FEED, CARRIAGE_RETURN => {},
             else => {
@@ -65,12 +71,12 @@ pub fn escape(bytes: []const u8, writer: anytype, formatter: anytype) !void {
             },
         }
 
-        // Write any non-escaped characters that have been buffered up until
-        // this point.
+        // Write all of the buffered non-escaped code points.
         if (start < i) {
             try formatter.writeRawFragment(writer, bytes[start..i]);
         }
 
+        // Escape and write the current code point.
         switch (byte) {
             DOUBLE_QUOTE, BACKSLASH, BACKSPACE, TAB, NEWLINE, FORM_FEED, CARRIAGE_RETURN => {
                 try formatter.writeCharEscape(writer, byte);
@@ -87,6 +93,8 @@ pub fn escape(bytes: []const u8, writer: anytype, formatter: anytype) !void {
         start = i + 1;
     }
 
+    // If there were no code points that required escaping in the input string,
+    // then write out all of the buffered non-escaped code points.
     if (start != bytes.len) {
         try formatter.writeRawFragment(writer, bytes[start..]);
     }
