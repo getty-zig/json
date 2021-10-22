f57b6b908f511ac4da852c5060958616167b3d34
Jason Phan <jason@ket.so>
2021-10-21 20:52:45 -0500

    de: Reorganize impls for Deserializer

--- src/de/deserializer.zig
+++ src/de/deserializer.zig
@@ -7,6 +7,7 @@ pub const Deserializer = struct {
     tokens: std.json.TokenStream,
 
     const Self = @This();
+    const impl = @"impl Deserializer";
 
     pub fn init(slice: []const u8) Self {
         return Self{ .tokens = std.json.TokenStream.init(slice) };
@@ -42,258 +43,261 @@ pub const Deserializer = struct {
     /// This method should always be called after a value has been fully
     /// deserialized.
     pub fn end(self: *Self) !void {
-        if (self.tokens.next() catch return Error.Input) |_| return Error.Input else {}
+        if (self.tokens.next() catch return impl.deserializer.Error.Input) |_| {
+            return impl.deserializer.Error.Input;
+        }
     }
 
-    /// Implements `getty.Deserializer`.
     pub usingnamespace getty.Deserializer(
         *Self,
-        Error,
-        deserializeBool,
-        deserializeEnum,
-        deserializeFloat,
-        deserializeInt,
-        deserializeMap,
-        deserializeOptional,
-        deserializeSequence,
-        deserializeString,
-        deserializeStruct,
-        deserializeVoid,
+        impl.deserializer.Error,
+        impl.deserializer.deserializeBool,
+        impl.deserializer.deserializeEnum,
+        impl.deserializer.deserializeFloat,
+        impl.deserializer.deserializeInt,
+        impl.deserializer.deserializeMap,
+        impl.deserializer.deserializeOptional,
+        impl.deserializer.deserializeSequence,
+        impl.deserializer.deserializeString,
+        impl.deserializer.deserializeStruct,
+        impl.deserializer.deserializeVoid,
     );
-
-    const Error = getty.de.Error || error{Input};
-    const Interface = @typeInfo(@TypeOf(Self.deserializer)).Fn.return_type.?;
-
-    /// Hint that the type being deserialized into is expecting a `bool` value.
-    fn deserializeBool(self: *Self, visitor: anytype) Error!@TypeOf(visitor).Value {
-        if (self.tokens.next() catch return Error.Input) |token| {
-            switch (token) {
-                .True => return try visitor.visitBool(Error, true),
-                .False => return try visitor.visitBool(Error, false),
-                else => {},
-            }
-        }
-
-        return Error.Input;
-    }
-
-    /// Hint that the type being deserialized into is expecting an `enum`
-    /// value.
-    fn deserializeEnum(self: *Self, visitor: anytype) Error!@TypeOf(visitor).Value {
-        if (self.tokens.next() catch return Error.Input) |token| {
-            switch (token) {
-                .Number => |num| {
-                    const slice = num.slice(self.tokens.slice, self.tokens.i - 1);
-
-                    if (num.is_integer) {
-                        return try switch (slice[0]) {
-                            '-' => visitor.visitInt(Error, parseSigned(slice) catch return Error.Input),
-                            else => visitor.visitInt(Error, parseUnsigned(slice) catch return Error.Input),
-                        };
-                    }
-                },
-                .String => |str| return try visitor.visitString(
-                    Error,
-                    str.slice(self.tokens.slice, self.tokens.i - 1),
-                ),
-                else => {},
-            }
-        }
-
-        return Error.Input;
-    }
-
-    /// Hint that the type being deserialized into is expecting a
-    /// floating-point value.
-    fn deserializeFloat(self: *Self, visitor: anytype) Error!@TypeOf(visitor).Value {
-        if (self.tokens.next() catch return Error.Input) |token| {
-            switch (token) {
-                .Number => |num| {
-                    const slice = num.slice(self.tokens.slice, self.tokens.i - 1);
-
-                    return try visitor.visitFloat(
-                        Error,
-                        std.fmt.parseFloat(f128, slice) catch return Error.Input,
-                    );
-                },
-                else => {},
-            }
-        }
-
-        return Error.Input;
-    }
-
-    /// Hint that the type being deserialized into is expecting an
-    /// integer value.
-    fn deserializeInt(self: *Self, visitor: anytype) Error!@TypeOf(visitor).Value {
-        if (self.tokens.next() catch return Error.Input) |token| {
-            switch (token) {
-                .Number => |num| {
-                    const slice = num.slice(self.tokens.slice, self.tokens.i - 1);
-
-                    switch (num.is_integer) {
-                        true => return try switch (slice[0]) {
-                            '-' => visitor.visitInt(Error, parseSigned(slice) catch return Error.Input),
-                            else => visitor.visitInt(Error, parseUnsigned(slice) catch return Error.Input),
-                        },
-                        false => {},
-                    }
-                },
-                else => {},
-            }
-        }
-
-        return Error.Input;
-    }
-
-    /// Hint that the type being deserialized into is expecting a map of
-    /// key-value pairs.
-    fn deserializeMap(self: *Self, visitor: anytype) Error!@TypeOf(visitor).Value {
-        if (self.tokens.next() catch return Error.Input) |token| {
-            if (token == .ObjectBegin) {
-                var access = MapAccess(Interface, Error){
-                    .allocator = self.allocator,
-                    .d = self.deserializer(),
-                };
-
-                return try visitor.visitMap(access.mapAccess());
-            }
-        }
-
-        return Error.Input;
-    }
-
-    /// Hint that the type being deserialized into is expecting an optional
-    /// value.
-    fn deserializeOptional(self: *Self, visitor: anytype) Error!@TypeOf(visitor).Value {
-        const tokens = self.tokens;
-
-        if (self.tokens.next() catch return Error.Input) |token| {
-            return try switch (token) {
-                .Null => visitor.visitNull(Error),
-                else => blk: {
-                    // Get back the token we just ate if it was an
-                    // actual value so that whenever the next
-                    // deserialize method is called by visitSome,
-                    // they'll eat the token we just saw instead of
-                    // whatever is after it.
-                    self.tokens = tokens;
-                    break :blk visitor.visitSome(self.deserializer());
-                },
-            };
-        }
-
-        return Error.Input;
-    }
-
-    /// Hint that the type being deserialized into is expecting a sequence of
-    /// values.
-    fn deserializeSequence(self: *Self, visitor: anytype) Error!@TypeOf(visitor).Value {
-        if (self.tokens.next() catch return Error.Input) |token| {
-            if (token == .ArrayBegin) {
-                var access = SequenceAccess(Interface, Error){
-                    .allocator = self.allocator,
-                    .d = self.deserializer(),
-                };
-
-                return try visitor.visitSequence(access.sequenceAccess());
-            }
-        }
-
-        return Error.Input;
-    }
-
-    /// Hint that the type being deserialized into is expecting a string value.
-    fn deserializeString(self: *Self, visitor: anytype) Error!@TypeOf(visitor).Value {
-        if (self.tokens.next() catch return Error.Input) |token| {
-            switch (token) {
-                .String => |str| return try visitor.visitString(
-                    Error,
-                    str.slice(self.tokens.slice, self.tokens.i - 1),
-                ),
-                else => {},
-            }
-        }
-
-        return Error.Input;
-    }
-
-    /// Hint that the type being deserialized into is expecting a struct value.
-    fn deserializeStruct(self: *Self, visitor: anytype) Error!@TypeOf(visitor).Value {
-        return try deserializeMap(self, visitor);
-    }
-
-    /// Hint that the type being deserialized into is expecting a `void` value.
-    fn deserializeVoid(self: *Self, visitor: anytype) Error!@TypeOf(visitor).Value {
-        if (self.tokens.next() catch return Error.Input) |token| {
-            if (token == .Null) {
-                return try visitor.visitVoid(Error);
-            }
-        }
-
-        return Error.Input;
-    }
 };
 
-fn SequenceAccess(comptime D: type, comptime Error: type) type {
-    return struct {
-        allocator: ?*std.mem.Allocator,
-        d: D,
+const @"impl Deserializer" = struct {
+    const deserializer = struct {
+        const Error = getty.de.Error || error{Input};
 
-        /// Implements `getty.de.SequenceAccess`.
-        pub usingnamespace getty.de.SequenceAccess(
-            *@This(),
-            Error,
-            nextElementSeed,
-        );
-
-        fn nextElementSeed(self: *@This(), seed: anytype) Error!?@TypeOf(seed).Value {
-            const tokens = self.d.context.tokens;
-
-            if (self.d.context.tokens.next() catch return Error.Input) |token| {
-                if (token == .ArrayEnd) {
-                    return null;
+        /// Hint that the type being deserialized into is expecting a `bool` value.
+        fn deserializeBool(self: *Deserializer, visitor: anytype) Error!@TypeOf(visitor).Value {
+            if (self.tokens.next() catch return Error.Input) |token| {
+                switch (token) {
+                    .True => return try visitor.visitBool(Error, true),
+                    .False => return try visitor.visitBool(Error, false),
+                    else => {},
                 }
-            } else {
-                return Error.Input;
             }
 
-            self.d.context.tokens = tokens;
-            return try seed.deserialize(self.allocator, self.d);
+            return Error.Input;
         }
-    };
-}
 
-fn MapAccess(comptime D: type, comptime Error: type) type {
-    return struct {
-        allocator: ?*std.mem.Allocator,
-        d: D,
+        /// Hint that the type being deserialized into is expecting an `enum`
+        /// value.
+        fn deserializeEnum(self: *Deserializer, visitor: anytype) Error!@TypeOf(visitor).Value {
+            if (self.tokens.next() catch return Error.Input) |token| {
+                switch (token) {
+                    .Number => |num| {
+                        const slice = num.slice(self.tokens.slice, self.tokens.i - 1);
 
-        /// Implements `getty.de.MapAccess`.
-        pub usingnamespace getty.de.MapAccess(
-            *@This(),
-            Error,
-            nextKeySeed,
-            nextValueSeed,
-        );
+                        if (num.is_integer) {
+                            return try switch (slice[0]) {
+                                '-' => visitor.visitInt(Error, parseSigned(slice) catch return Error.Input),
+                                else => visitor.visitInt(Error, parseUnsigned(slice) catch return Error.Input),
+                            };
+                        }
+                    },
+                    .String => |str| return try visitor.visitString(
+                        Error,
+                        str.slice(self.tokens.slice, self.tokens.i - 1),
+                    ),
+                    else => {},
+                }
+            }
 
-        fn nextKeySeed(self: *@This(), seed: anytype) Error!?@TypeOf(seed).Value {
-            if (self.d.context.tokens.next() catch return Error.Input) |token| {
-                return switch (token) {
-                    .ObjectEnd => null,
-                    .String => |str| str.slice(self.d.context.tokens.slice, self.d.context.tokens.i - 1),
-                    else => Error.Input,
+            return Error.Input;
+        }
+
+        /// Hint that the type being deserialized into is expecting a
+        /// floating-point value.
+        fn deserializeFloat(self: *Deserializer, visitor: anytype) Error!@TypeOf(visitor).Value {
+            if (self.tokens.next() catch return Error.Input) |token| {
+                switch (token) {
+                    .Number => |num| {
+                        const slice = num.slice(self.tokens.slice, self.tokens.i - 1);
+
+                        return try visitor.visitFloat(
+                            Error,
+                            std.fmt.parseFloat(f128, slice) catch return Error.Input,
+                        );
+                    },
+                    else => {},
+                }
+            }
+
+            return Error.Input;
+        }
+
+        /// Hint that the type being deserialized into is expecting an
+        /// integer value.
+        fn deserializeInt(self: *Deserializer, visitor: anytype) Error!@TypeOf(visitor).Value {
+            if (self.tokens.next() catch return Error.Input) |token| {
+                switch (token) {
+                    .Number => |num| {
+                        const slice = num.slice(self.tokens.slice, self.tokens.i - 1);
+
+                        switch (num.is_integer) {
+                            true => return try switch (slice[0]) {
+                                '-' => visitor.visitInt(Error, parseSigned(slice) catch return Error.Input),
+                                else => visitor.visitInt(Error, parseUnsigned(slice) catch return Error.Input),
+                            },
+                            false => {},
+                        }
+                    },
+                    else => {},
+                }
+            }
+
+            return Error.Input;
+        }
+
+        /// Hint that the type being deserialized into is expecting a map of
+        /// key-value pairs.
+        fn deserializeMap(self: *Deserializer, visitor: anytype) Error!@TypeOf(visitor).Value {
+            if (self.tokens.next() catch return Error.Input) |token| {
+                if (token == .ObjectBegin) {
+                    var access = Access{
+                        .allocator = self.allocator,
+                        .deserializer = self,
+                    };
+
+                    return try visitor.visitMap(access.mapAccess());
+                }
+            }
+
+            return Error.Input;
+        }
+
+        /// Hint that the type being deserialized into is expecting an optional
+        /// value.
+        fn deserializeOptional(self: *Deserializer, visitor: anytype) Error!@TypeOf(visitor).Value {
+            const tokens = self.tokens;
+
+            if (self.tokens.next() catch return Error.Input) |token| {
+                return try switch (token) {
+                    .Null => visitor.visitNull(Error),
+                    else => blk: {
+                        // Get back the token we just ate if it was an actual
+                        // value so that whenever the next deserialize method
+                        // is called by visitSome, it'll eat the token we just
+                        // saw instead of whatever comes after it.
+                        self.tokens = tokens;
+                        break :blk visitor.visitSome(self.deserializer());
+                    },
                 };
             }
 
             return Error.Input;
         }
 
-        fn nextValueSeed(self: *@This(), seed: anytype) Error!@TypeOf(seed).Value {
-            return try seed.deserialize(self.allocator, self.d);
+        /// Hint that the type being deserialized into is expecting a sequence of
+        /// values.
+        fn deserializeSequence(self: *Deserializer, visitor: anytype) Error!@TypeOf(visitor).Value {
+            if (self.tokens.next() catch return Error.Input) |token| {
+                if (token == .ArrayBegin) {
+                    var access = Access{
+                        .allocator = self.allocator,
+                        .deserializer = self,
+                    };
+
+                    return try visitor.visitSequence(access.sequenceAccess());
+                }
+            }
+
+            return Error.Input;
+        }
+
+        /// Hint that the type being deserialized into is expecting a string value.
+        fn deserializeString(self: *Deserializer, visitor: anytype) Error!@TypeOf(visitor).Value {
+            if (self.tokens.next() catch return Error.Input) |token| {
+                switch (token) {
+                    .String => |str| return try visitor.visitString(
+                        Error,
+                        str.slice(self.tokens.slice, self.tokens.i - 1),
+                    ),
+                    else => {},
+                }
+            }
+
+            return Error.Input;
+        }
+
+        /// Hint that the type being deserialized into is expecting a struct value.
+        fn deserializeStruct(self: *Deserializer, visitor: anytype) Error!@TypeOf(visitor).Value {
+            return try deserializeMap(self, visitor);
+        }
+
+        /// Hint that the type being deserialized into is expecting a `void` value.
+        fn deserializeVoid(self: *Deserializer, visitor: anytype) Error!@TypeOf(visitor).Value {
+            if (self.tokens.next() catch return Error.Input) |token| {
+                if (token == .Null) {
+                    return try visitor.visitVoid(Error);
+                }
+            }
+
+            return Error.Input;
         }
     };
-}
+};
+
+const Access = struct {
+    allocator: ?*std.mem.Allocator,
+    deserializer: *Deserializer,
+
+    const Self = @This();
+    const impl = @"impl Access";
+
+    const Error = @"impl Deserializer".deserializer.Error;
+
+    pub usingnamespace getty.de.SequenceAccess(
+        *Self,
+        Error,
+        impl.sequenceAccess.nextElementSeed,
+    );
+
+    pub usingnamespace getty.de.MapAccess(
+        *Self,
+        Error,
+        impl.mapAccess.nextKeySeed,
+        impl.mapAccess.nextValueSeed,
+    );
+};
+
+const @"impl Access" = struct {
+    const sequenceAccess = struct {
+        fn nextElementSeed(self: *Access, seed: anytype) Access.Error!?@TypeOf(seed).Value {
+            const tokens = self.deserializer.tokens;
+
+            if (self.deserializer.tokens.next() catch return error.Input) |token| {
+                if (token == .ArrayEnd) {
+                    return null;
+                }
+            } else {
+                return error.Input;
+            }
+
+            self.deserializer.tokens = tokens;
+            return try seed.deserialize(self.allocator, self.deserializer.deserializer());
+        }
+    };
+
+    const mapAccess = struct {
+        fn nextKeySeed(self: *Access, seed: anytype) Access.Error!?@TypeOf(seed).Value {
+            if (self.deserializer.tokens.next() catch return error.Input) |token| {
+                return switch (token) {
+                    .ObjectEnd => null,
+                    .String => |str| str.slice(self.deserializer.tokens.slice, self.deserializer.tokens.i - 1),
+                    else => error.Input,
+                };
+            }
+
+            return error.Input;
+        }
+
+        fn nextValueSeed(self: *Access, seed: anytype) Access.Error!@TypeOf(seed).Value {
+            return try seed.deserialize(self.allocator, self.deserializer.deserializer());
+        }
+    };
+};
 
 fn parseInt(comptime T: type, buf: []const u8) std.fmt.ParseIntError!T {
     comptime std.debug.assert(T == u64 or T == i64);
