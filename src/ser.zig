0d3adf05310fe242ad60a7a449b1bea2210bbc26
Jason Phan <jason@ket.so>
2021-11-03 12:18:10 -0500

    ser: Fix absolute value computation for int formatting
    The former method didn't allow for formatting the minimum possible
    values of signed integer types.

--- src/ser.zig
+++ src/ser.zig
@@ -138,11 +138,26 @@ test "toWriter - HashMap" {
 }
 
 test "toWriter - Integer" {
-    try t(.compact, 'A', "65");
+    try t(.compact, 1, "1");
+    try t(.compact, -1, "-1");
+
+    try t(.compact, std.math.maxInt(u8), "255");
     try t(.compact, std.math.maxInt(u32), "4294967295");
     try t(.compact, std.math.maxInt(u64), "18446744073709551615");
-    try t(.compact, std.math.minInt(i32), "-2147483648");
+    try t(.compact, std.math.maxInt(u128), "340282366920938463463374607431768211455");
+
+    try t(.compact, std.math.maxInt(i8), "127");
+    try t(.compact, std.math.maxInt(i32), "2147483647");
     try t(.compact, std.math.maxInt(i64), "9223372036854775807");
+    try t(.compact, std.math.maxInt(i128), "170141183460469231731687303715884105727");
+    try t(.compact, std.math.minInt(i8) + 1, "-127");
+    try t(.compact, std.math.minInt(i32) + 1, "-2147483647");
+    try t(.compact, std.math.minInt(i64) + 1, "-9223372036854775807");
+    try t(.compact, std.math.minInt(i128) + 1, "-170141183460469231731687303715884105727");
+    try t(.compact, std.math.minInt(i8), "-128");
+    try t(.compact, std.math.minInt(i32), "-2147483648");
+    try t(.compact, std.math.minInt(i64), "-9223372036854775808");
+    try t(.compact, std.math.minInt(i128), "-170141183460469231731687303715884105728");
 }
 
 test "toWriter - Float" {
--- src/ser/impl/formatter/details/fmt.zig
+++ src/ser/impl/formatter/details/fmt.zig
@@ -1,36 +1,88 @@
 const std = @import("std");
 
-// Converts values in the range [0, 100) to a string.
-pub fn digits2(value: usize) []const u8 {
-    const digits =
-        "0001020304050607080910111213141516171819" ++
-        "2021222324252627282930313233343536373839" ++
-        "4041424344454647484950515253545556575859" ++
-        "6061626364656667686970717273747576777879" ++
-        "8081828384858687888990919293949596979899";
+const math = std.math;
 
-    return digits[value * 2 ..];
-}
+const maxInt = math.maxInt;
+const minInt = math.minInt;
 
-/// Returns the number of digits in `value`.
-pub fn countDigits(value: anytype) usize {
+pub fn formatInt(value: anytype, writer: anytype) !void {
     const T = @TypeOf(value);
     const info = @typeInfo(T);
 
     comptime std.debug.assert(info == .Int);
 
-    const bits = comptime std.math.log2(10);
-    var n: usize = 1;
+    // This buffer should be large enough to hold all digits of T and a sign.
+    //
+    // TODO: Change to digits10 + 1 for better space efficiency.
+    var buf: [math.max(std.meta.bitCount(T), 1) + 1]u8 = undefined;
 
-    {
-        var v = value >> bits;
-        while (v != 0) : (n += 1) v >>= bits;
-    }
+    var start = switch (info.Int.signedness) {
+        .signed => blk: {
+            var start = try formatDecimal(abs(value), &buf);
 
-    return n;
+            if (value < 0) {
+                start -= 1;
+                buf[start] = '-';
+            }
+
+            break :blk start;
+        },
+        .unsigned => try formatDecimal(value, &buf),
+    };
+
+    try writer.writeAll(buf[start..]);
 }
 
-pub fn formatDecimal(value: anytype, buf: []u8) !usize {
+/// Returns the absolute value of `x`.
+///
+/// The return type of `abs` is the smallest type between u32, u64, and u128
+/// that can store all possible absolute values of the type of `x`. For
+/// example, if the type of `x` is i8, then the greatest absolute value
+/// possible is 128. 128 can be stored as a u32, and u32 is smaller than u64
+/// and u128, so u32 is the return type.
+///
+/// For positive integers, `x` is simply casted to the return type and
+/// returned.
+///
+/// For negative integers, if `x` is greater than the smallest possible value
+/// of @TypeOf(x), then the negation of `x` is casted to the return type and is
+/// returned. Otherwise, the wrapped difference between `x` and 1 is casted to
+/// the return type and the sum between the difference and 1 is returned.
+fn abs(x: anytype) U32OrU64OrU128(@TypeOf(x)) {
+    comptime std.debug.assert(@typeInfo(@TypeOf(x)) == .Int);
+
+    const Return = U32OrU64OrU128(@TypeOf(x));
+
+    if (x > 0) return @intCast(Return, x);
+
+    if (x > minInt(@TypeOf(x))) {
+        return @intCast(Return, -x);
+    } else {
+        return @intCast(Return, x -% 1) + 1;
+    }
+}
+
+/// Returns the smallest type between u8, u32, u64, and u128 that can hold all
+/// positive values of T.
+fn U32OrU64OrU128(comptime T: type) type {
+    comptime std.debug.assert(@typeInfo(T) == .Int);
+    comptime std.debug.assert(@typeInfo(T).Int.bits <= 128);
+
+    const max = maxInt(T);
+    const max_u32 = maxInt(u32);
+    const max_u64 = maxInt(u64);
+    const max_u128 = maxInt(u128);
+
+    if (max <= max_u32) return u32;
+    if (max <= max_u64) return u64;
+    if (max <= max_u128) return u128;
+
+    // UNREACHABLE: It is asserted earlier in the function that the number of
+    // bits in T is less than or equal to 128.
+    unreachable;
+}
+
+fn formatDecimal(value: anytype, buf: []u8) !usize {
     const info = @typeInfo(@TypeOf(value));
 
     comptime std.debug.assert(info == .Int);
@@ -54,32 +106,32 @@ pub fn formatDecimal(value: anytype, buf: []u8) !usize {
     return index;
 }
 
-pub fn formatInt(value: anytype, writer: anytype) !void {
+/// Converts values in the range [0, 100) to a string.
+fn digits2(value: usize) []const u8 {
+    const digits =
+        "0001020304050607080910111213141516171819" ++
+        "2021222324252627282930313233343536373839" ++
+        "4041424344454647484950515253545556575859" ++
+        "6061626364656667686970717273747576777879" ++
+        "8081828384858687888990919293949596979899";
+
+    return digits[value * 2 ..];
+}
+
+/// Returns the number of digits in `value`.
+fn countDigits(value: anytype) usize {
     const T = @TypeOf(value);
     const info = @typeInfo(T);
 
-    comptime std.debug.assert(info == .Int or info == .ComptimeInt);
+    comptime std.debug.assert(info == .Int);
 
-    // This buffer should be large enough to hold all digits and a sign.
-    //
-    // TODO: Change to digits10 + 1.
-    var buf: [std.math.max(std.meta.bitCount(T), 1) + 1]u8 = undefined;
+    const bits = comptime std.math.log2(10);
+    var n: usize = 1;
 
-    var start = switch (info.Int.signedness) {
-        .signed => blk: {
-            const abs = std.math.absInt(@as(i128, value)) catch unreachable; // TODO: change unreachable
+    {
+        var v = value >> bits;
+        while (v != 0) : (n += 1) v >>= bits;
+    }
 
-            var start = try formatDecimal(abs, &buf);
-
-            if (value < 0) {
-                start -= 1;
-                buf[start] = '-';
-            }
-
-            break :blk start;
-        },
-        .unsigned => try formatDecimal(value, &buf),
-    };
-
-    try writer.writeAll(buf[start..]);
+    return n;
 }
