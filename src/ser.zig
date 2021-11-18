e6d8c8c3c2c8466848666268ca435c8712e9a57a
Jason Phan <jason@ket.so>
2021-11-18 17:06:56 -0600

    ser: Move concepts to bottom of file

--- src/ser.zig
+++ src/ser.zig
@@ -10,41 +10,6 @@ pub const ser = struct {
     pub usingnamespace @import("ser/impl/formatter/pretty.zig");
 };
 
-const concepts = struct {
-    fn @"std.io.Writer"(comptime T: type) void {
-        const err = "expected `std.io.Writer` interface value, found `" ++ @typeName(T) ++ "`";
-
-        comptime {
-            // Invariants
-            if (!std.meta.trait.isContainer(T)) {
-                @compileError(err);
-            }
-
-            // Constraints
-            const has_name = std.mem.startsWith(u8, @typeName(T), "std.io.writer.Writer");
-            const has_field = std.meta.trait.hasField("context")(T);
-            const has_decl = @hasDecl(T, "Error");
-            const has_funcs = std.meta.trait.hasFunctions(T, .{
-                "write",
-                "writeAll",
-                "print",
-                "writeByte",
-                "writeByteNTimes",
-                "writeIntNative",
-                "writeIntForeign",
-                "writeIntLittle",
-                "writeIntBig",
-                "writeInt",
-                "writeStruct",
-            });
-
-            if (!(has_name and has_field and has_decl and has_funcs)) {
-                @compileError(err);
-            }
-        }
-    }
-};
-
 /// Serialize the given value as JSON into the given I/O stream.
 pub fn toWriter(value: anytype, writer: anytype) !void {
     comptime concepts.@"std.io.Writer"(@TypeOf(writer));
@@ -142,6 +107,41 @@ pub fn toPrettySliceWith(allocator: *std.mem.Allocator, value: anytype, _ser: an
     try toPrettyWriterWith(value, list.writer(), _ser);
     return list.toOwnedSlice();
 }
+
+const concepts = struct {
+    fn @"std.io.Writer"(comptime T: type) void {
+        const err = "expected `std.io.Writer` interface value, found `" ++ @typeName(T) ++ "`";
+
+        comptime {
+            // Invariants
+            if (!std.meta.trait.isContainer(T)) {
+                @compileError(err);
+            }
+
+            // Constraints
+            const has_name = std.mem.startsWith(u8, @typeName(T), "std.io.writer.Writer");
+            const has_field = std.meta.trait.hasField("context")(T);
+            const has_decl = @hasDecl(T, "Error");
+            const has_funcs = std.meta.trait.hasFunctions(T, .{
+                "write",
+                "writeAll",
+                "print",
+                "writeByte",
+                "writeByteNTimes",
+                "writeIntNative",
+                "writeIntForeign",
+                "writeIntLittle",
+                "writeIntBig",
+                "writeInt",
+                "writeStruct",
+            });
+
+            if (!(has_name and has_field and has_decl and has_funcs)) {
+                @compileError(err);
+            }
+        }
+    }
+};
 
 test "toWriter - Array" {
     try t(.compact, [_]i8{}, "[]");
