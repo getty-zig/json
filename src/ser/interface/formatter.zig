202b6efbc4365ad9e1803936311485e1c46655dc
Jason Phan <jason@ket.so>
2022-11-17 21:45:15 -0600

    ser: Update Formatter interface with struct for methods

--- src/ser/interface/formatter.zig
+++ src/ser/interface/formatter.zig
@@ -1,26 +1,30 @@
 pub fn Formatter(
     comptime Context: type,
     comptime Writer: type,
-    comptime nullFn: fn (Context, Writer) Writer.Error!void,
-    comptime boolFn: fn (Context, Writer, bool) Writer.Error!void,
-    comptime intFn: fn (Context, Writer, anytype) Writer.Error!void,
-    comptime floatFn: fn (Context, Writer, anytype) Writer.Error!void,
-    comptime numberStringFn: fn (Context, Writer, []const u8) Writer.Error!void,
-    comptime beginStringFn: fn (Context, Writer) Writer.Error!void,
-    comptime endStringFn: fn (Context, Writer) Writer.Error!void,
-    comptime stringFragmentFn: fn (Context, Writer, []const u8) Writer.Error!void,
-    comptime charEscapeFn: fn (Context, Writer, u21) Writer.Error!void,
-    comptime beginArrayFn: fn (Context, Writer) Writer.Error!void,
-    comptime endArrayFn: fn (Context, Writer) Writer.Error!void,
-    comptime beginArrayValueFn: fn (Context, Writer, bool) Writer.Error!void,
-    comptime endArrayValueFn: fn (Context, Writer) Writer.Error!void,
-    comptime beginObjectFn: fn (Context, Writer) Writer.Error!void,
-    comptime endObjectFn: fn (Context, Writer) Writer.Error!void,
-    comptime beginObjectKeyFn: fn (Context, Writer, bool) Writer.Error!void,
-    comptime endObjectKeyFn: fn (Context, Writer) Writer.Error!void,
-    comptime beginObjectValueFn: fn (Context, Writer) Writer.Error!void,
-    comptime endObjectValueFn: fn (Context, Writer) Writer.Error!void,
-    comptime rawFragmentFn: fn (Context, Writer, []const u8) Writer.Error!void,
+    comptime impls: struct {
+        writeBool: ?fn (Context, Writer, bool) Writer.Error!void = null,
+        writeCharEscape: ?fn (Context, Writer, u21) Writer.Error!void = null,
+        writeInt: ?fn (Context, Writer, anytype) Writer.Error!void = null,
+        writeFloat: ?fn (Context, Writer, anytype) Writer.Error!void = null,
+        writeNull: ?fn (Context, Writer) Writer.Error!void = null,
+        writeNumberString: ?fn (Context, Writer, []const u8) Writer.Error!void = null,
+        writeRawFragment: ?fn (Context, Writer, []const u8) Writer.Error!void = null,
+        writeStringFragment: ?fn (Context, Writer, []const u8) Writer.Error!void = null,
+
+        beginArray: ?fn (Context, Writer) Writer.Error!void = null,
+        beginArrayValue: ?fn (Context, Writer, bool) Writer.Error!void = null,
+        beginObject: ?fn (Context, Writer) Writer.Error!void = null,
+        beginObjectKey: ?fn (Context, Writer, bool) Writer.Error!void = null,
+        beginObjectValue: ?fn (Context, Writer) Writer.Error!void = null,
+        beginString: ?fn (Context, Writer) Writer.Error!void = null,
+
+        endArray: ?fn (Context, Writer) Writer.Error!void = null,
+        endArrayValue: ?fn (Context, Writer) Writer.Error!void = null,
+        endString: ?fn (Context, Writer) Writer.Error!void = null,
+        endObject: ?fn (Context, Writer) Writer.Error!void = null,
+        endObjectKey: ?fn (Context, Writer) Writer.Error!void = null,
+        endObjectValue: ?fn (Context, Writer) Writer.Error!void = null,
+    },
 ) type {
     const T = struct {
         context: Context,
@@ -29,27 +33,43 @@ pub fn Formatter(
 
         /// Writes a `null` value to the specified writer.
         pub fn writeNull(self: Self, writer: Writer) Writer.Error!void {
-            try nullFn(self.context, writer);
+            if (impls.writeNull) |f| {
+                try f(self.context, writer);
+            } else {
+                @compileError("writeNull is not implemented by type: " ++ @typeName(Context));
+            }
         }
 
         /// Writes `true` or `false` to the specified writer.
         pub fn writeBool(self: Self, writer: Writer, value: bool) Writer.Error!void {
-            try boolFn(self.context, writer, value);
-        }
-
-        /// Writes an integer value to the specified writer.
-        pub fn writeInt(self: Self, writer: Writer, value: anytype) Writer.Error!void {
-            switch (@typeInfo(@TypeOf(value))) {
-                .ComptimeInt, .Int => try intFn(self.context, writer, value),
-                else => @compileError("expected integer, found " ++ @typeName(@TypeOf(value))),
+            if (impls.writeBool) |f| {
+                try f(self.context, writer, value);
+            } else {
+                @compileError("writeBool is not implemented by type: " ++ @typeName(Context));
             }
         }
 
         // Writes an floating point value to the specified writer.
         pub fn writeFloat(self: Self, writer: Writer, value: anytype) Writer.Error!void {
-            switch (@typeInfo(@TypeOf(value))) {
-                .ComptimeFloat, .Float => try floatFn(self.context, writer, value),
-                else => @compileError("expected floating point, found " ++ @typeName(@TypeOf(value))),
+            if (impls.writeFloat) |f| {
+                switch (@typeInfo(@TypeOf(value))) {
+                    .ComptimeFloat, .Float => try f(self.context, writer, value),
+                    else => @compileError("expected float, found " ++ @typeName(@TypeOf(value))),
+                }
+            } else {
+                @compileError("writeFloat is not implemented by type: " ++ @typeName(Context));
+            }
+        }
+
+        /// Writes an integer value to the specified writer.
+        pub fn writeInt(self: Self, writer: Writer, value: anytype) Writer.Error!void {
+            if (impls.writeInt) |f| {
+                switch (@typeInfo(@TypeOf(value))) {
+                    .ComptimeInt, .Int => try f(self.context, writer, value),
+                    else => @compileError("expected integer, found " ++ @typeName(@TypeOf(value))),
+                }
+            } else {
+                @compileError("writeInt is not implemented by type: " ++ @typeName(Context));
             }
         }
 
@@ -57,73 +77,137 @@ pub fn Formatter(
         ///
         /// TODO: Check that the string is actually an integer when parsed.
         pub fn writeNumberString(self: Self, writer: Writer, value: []const u8) Writer.Error!void {
-            try numberStringFn(self.context, writer, value);
+            if (impls.writeNumberString) |f| {
+                try f(self.context, writer, value);
+            } else {
+                @compileError("writeNumberString is not implemented by type: " ++ @typeName(Context));
+            }
         }
 
         /// Called before each series of `write_string_fragment` and
         /// `write_char_escape`.  Writes a `"` to the specified writer.
         pub fn beginString(self: Self, writer: Writer) Writer.Error!void {
-            try beginStringFn(self.context, writer);
+            if (impls.beginString) |f| {
+                try f(self.context, writer);
+            } else {
+                @compileError("beginString is not implemented by type: " ++ @typeName(Context));
+            }
         }
 
         /// Called after each series of `write_string_fragment` and
         /// `write_char_escape`.  Writes a `"` to the specified writer.
         pub fn endString(self: Self, writer: Writer) Writer.Error!void {
-            try endStringFn(self.context, writer);
+            if (impls.endString) |f| {
+                try f(self.context, writer);
+            } else {
+                @compileError("endString is not implemented by type: " ++ @typeName(Context));
+            }
         }
 
         /// Writes a string fragment that doesn't need any escaping to the
         /// specified writer.
         pub fn writeStringFragment(self: Self, writer: Writer, value: []const u8) Writer.Error!void {
-            try stringFragmentFn(self.context, writer, value);
+            if (impls.writeStringFragment) |f| {
+                try f(self.context, writer, value);
+            } else {
+                @compileError("writeStringFragment is not implemented by type: " ++ @typeName(Context));
+            }
         }
 
         pub fn writeCharEscape(self: Self, writer: Writer, value: u21) Writer.Error!void {
-            try charEscapeFn(self.context, writer, value);
+            if (impls.writeCharEscape) |f| {
+                try f(self.context, writer, value);
+            } else {
+                @compileError("writeCharEscape is not implemented by type: " ++ @typeName(Context));
+            }
         }
 
         pub fn beginArray(self: Self, writer: Writer) Writer.Error!void {
-            try beginArrayFn(self.context, writer);
+            if (impls.beginArray) |f| {
+                try f(self.context, writer);
+            } else {
+                @compileError("beginArray is not implemented by type: " ++ @typeName(Context));
+            }
         }
 
         pub fn endArray(self: Self, writer: Writer) Writer.Error!void {
-            try endArrayFn(self.context, writer);
+            if (impls.endArray) |f| {
+                try f(self.context, writer);
+            } else {
+                @compileError("endArray is not implemented by type: " ++ @typeName(Context));
+            }
         }
 
         pub fn beginArrayValue(self: Self, writer: Writer, first: bool) Writer.Error!void {
-            try beginArrayValueFn(self.context, writer, first);
+            if (impls.beginArrayValue) |f| {
+                try f(self.context, writer, first);
+            } else {
+                @compileError("beginArrayValue is not implemented by type: " ++ @typeName(Context));
+            }
         }
 
         pub fn endArrayValue(self: Self, writer: Writer) Writer.Error!void {
-            try endArrayValueFn(self.context, writer);
+            if (impls.endArrayValue) |f| {
+                try f(self.context, writer);
+            } else {
+                @compileError("endArrayValue is not implemented by type: " ++ @typeName(Context));
+            }
         }
 
         pub fn beginObject(self: Self, writer: Writer) Writer.Error!void {
-            try beginObjectFn(self.context, writer);
+            if (impls.beginObject) |f| {
+                try f(self.context, writer);
+            } else {
+                @compileError("beginObject is not implemented by type: " ++ @typeName(Context));
+            }
         }
 
         pub fn endObject(self: Self, writer: Writer) Writer.Error!void {
-            try endObjectFn(self.context, writer);
+            if (impls.endObject) |f| {
+                try f(self.context, writer);
+            } else {
+                @compileError("endObject is not implemented by type: " ++ @typeName(Context));
+            }
         }
 
         pub fn beginObjectKey(self: Self, writer: Writer, first: bool) Writer.Error!void {
-            try beginObjectKeyFn(self.context, writer, first);
+            if (impls.beginObjectKey) |f| {
+                try f(self.context, writer, first);
+            } else {
+                @compileError("beginObjectKey is not implemented by type: " ++ @typeName(Context));
+            }
         }
 
         pub fn endObjectKey(self: Self, writer: Writer) Writer.Error!void {
-            try endObjectKeyFn(self.context, writer);
+            if (impls.endObjectKey) |f| {
+                try f(self.context, writer);
+            } else {
+                @compileError("endObjectKey is not implemented by type: " ++ @typeName(Context));
+            }
         }
 
         pub fn beginObjectValue(self: Self, writer: Writer) Writer.Error!void {
-            try beginObjectValueFn(self.context, writer);
+            if (impls.beginObjectValue) |f| {
+                try f(self.context, writer);
+            } else {
+                @compileError("beginObjectValue is not implemented by type: " ++ @typeName(Context));
+            }
         }
 
         pub fn endObjectValue(self: Self, writer: Writer) Writer.Error!void {
-            try endObjectValueFn(self.context, writer);
+            if (impls.endObjectValue) |f| {
+                try f(self.context, writer);
+            } else {
+                @compileError("endObjectValue is not implemented by type: " ++ @typeName(Context));
+            }
         }
 
         pub fn writeRawFragment(self: Self, writer: Writer, value: []const u8) Writer.Error!void {
-            try rawFragmentFn(self.context, writer, value);
+            if (impls.writeRawFragment) |f| {
+                try f(self.context, writer, value);
+            } else {
+                @compileError("writeRawFragment is not implemented by type: " ++ @typeName(Context));
+            }
         }
     };
 
