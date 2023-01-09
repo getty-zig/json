ab3a412e3b2e31201e41843dd108f2fd65ce1a08
Jason Phan <jason@ket.so>
2023-01-09 09:55:59 -0600

    build: Fetch deps using GitRepoStep
    This enables getty-json to be built without Gyro or Zigmod.

--- build.zig
+++ build.zig
@@ -1,34 +1,61 @@
 const std = @import("std");
-const pkgs = @import("deps.zig").pkgs;
 
 const package_name = "json";
 const package_path = "src/json.zig";
 
-const json_pkg = std.build.Pkg{
-    .name = package_name,
-    .source = .{ .path = package_path },
-    .dependencies = &[_]std.build.Pkg{
-        pkgs.concepts,
-        pkgs.getty,
-    },
-};
-
-pub fn build(b: *std.build.Builder) void {
+pub fn build(b: *std.build.Builder) !void {
     const mode = b.standardReleaseOptions();
     const target = b.standardTargetOptions(.{});
 
-    tests(b, mode, target);
+    try tests(b, mode, target);
     docs(b);
     clean(b);
 }
 
-fn tests(b: *std.build.Builder, mode: std.builtin.Mode, target: std.zig.CrossTarget) void {
+fn tests(b: *std.build.Builder, mode: std.builtin.Mode, target: std.zig.CrossTarget) !void {
+    // Packages
+    const getty = GitRepoStep.create(b, .{
+        .url = "https://github.com/getty-zig/getty",
+        .branch = "develop",
+        .sha = "8b2464c4ec7a9bd91cf57582aebe356b551555b9",
+        .fetch_enabled = true,
+    });
+
+    const concepts = GitRepoStep.create(b, .{
+        .url = "https://github.com/ibokuri/concepts",
+        .branch = "main",
+        .sha = "05c73681bc131a32f71b1f330dc5f4bdcea53675",
+        .fetch_enabled = true,
+    });
+
     const test_all_step = b.step("test", "Run tests");
     const test_ser_step = b.step("test-ser", "Run serialization tests");
     const test_de_step = b.step("test-de", "Run deserialization tests");
 
-    addTest(b, mode, target, test_all_step, test_ser_step, "src/ser.zig");
-    addTest(b, mode, target, test_all_step, test_de_step, "src/de.zig");
+    // Configure tests.
+    const t_ser = b.addTest("src/ser.zig");
+    t_ser.setTarget(target);
+    t_ser.setBuildMode(mode);
+    t_ser.step.dependOn(&getty.step);
+    t_ser.step.dependOn(&concepts.step);
+    t_ser.addPackagePath("getty", try std.fs.path.join(b.allocator, &[_][]const u8{ getty.getPath(&t_ser.step), "src/getty.zig" }));
+    t_ser.addPackagePath("concepts", try std.fs.path.join(b.allocator, &[_][]const u8{ concepts.getPath(&t_ser.step), "src/lib.zig" }));
+
+    const t_de = b.addTest("src/de.zig");
+    t_de.setTarget(target);
+    t_de.setBuildMode(mode);
+    t_de.step.dependOn(&getty.step);
+    t_de.step.dependOn(&concepts.step);
+    t_de.addPackagePath("getty", try std.fs.path.join(b.allocator, &[_][]const u8{ getty.getPath(&t_ser.step), "src/getty.zig" }));
+    t_de.addPackagePath("concepts", try std.fs.path.join(b.allocator, &[_][]const u8{ concepts.getPath(&t_ser.step), "src/lib.zig" }));
+
+    // Configure module-level test steps.
+    test_ser_step.dependOn(&t_ser.step);
+    test_de_step.dependOn(&t_de.step);
+
+    // Configure top-level test step.
+    test_all_step.dependOn(test_ser_step);
+    test_all_step.dependOn(test_de_step);
 }
 
 fn docs(b: *std.build.Builder) void {
@@ -58,19 +85,205 @@ fn clean(b: *std.build.Builder) void {
     clean_step.dependOn(&cmd.step);
 }
 
-fn addTest(
-    b: *std.build.Builder,
-    mode: std.builtin.Mode,
-    target: std.zig.CrossTarget,
-    all_step: *std.build.Step,
-    step: *std.build.Step,
-    comptime path: []const u8,
-) void {
-    const t = b.addTest(path);
-    t.setTarget(target);
-    t.setBuildMode(mode);
-    pkgs.addAllTo(t);
+////////////////////////////////////////////////////////////////////////////////
+// GitRepoStep (copied from https://github.com/marler8997/zig-build-repos)
+////////////////////////////////////////////////////////////////////////////////
 
-    step.dependOn(&t.step);
-    all_step.dependOn(step);
-}
+const GitRepoStep = struct {
+    step: std.build.Step,
+    builder: *std.build.Builder,
+    url: []const u8,
+    name: []const u8,
+    branch: ?[]const u8 = null,
+    sha: []const u8,
+    path: []const u8,
+    sha_check: ShaCheck = .warn,
+    fetch_enabled: bool,
+
+    pub const ShaCheck = enum {
+        none,
+        warn,
+        err,
+
+        pub fn reportFail(self: ShaCheck, comptime fmt: []const u8, args: anytype) void {
+            switch (self) {
+                .none => unreachable,
+                .warn => std.log.warn(fmt, args),
+                .err => {
+                    std.log.err(fmt, args);
+                    std.os.exit(0xff);
+                },
+            }
+        }
+    };
+
+    var cached_default_fetch_option: ?bool = null;
+
+    pub fn defaultFetchOption(b: *std.build.Builder) bool {
+        if (cached_default_fetch_option) |_| {} else {
+            cached_default_fetch_option = if (b.option(bool, "fetch", "automatically fetch network resources")) |o| o else false;
+        }
+        return cached_default_fetch_option.?;
+    }
+
+    pub fn create(b: *std.build.Builder, opt: struct {
+        url: []const u8,
+        branch: ?[]const u8 = null,
+        sha: []const u8,
+        path: ?[]const u8 = null,
+        sha_check: ShaCheck = .warn,
+        fetch_enabled: ?bool = null,
+    }) *GitRepoStep {
+        var result = b.allocator.create(GitRepoStep) catch @panic("memory");
+        const name = std.fs.path.basename(opt.url);
+        result.* = GitRepoStep{
+            .step = std.build.Step.init(.custom, "clone a git repository", b.allocator, make),
+            .builder = b,
+            .url = opt.url,
+            .name = name,
+            .branch = opt.branch,
+            .sha = opt.sha,
+            .path = if (opt.path) |p| (b.allocator.dupe(u8, p) catch @panic("memory")) else (std.fs.path.resolve(b.allocator, &[_][]const u8{
+                b.build_root,
+                "lib",
+                name,
+            })) catch @panic("memory"),
+            .sha_check = opt.sha_check,
+            .fetch_enabled = if (opt.fetch_enabled) |fe| fe else defaultFetchOption(b),
+        };
+        return result;
+    }
+
+    fn hasDependency(step: *const std.build.Step, dep_candidate: *const std.build.Step) bool {
+        for (step.dependencies.items) |dep| {
+            if (dep == dep_candidate or hasDependency(dep, dep_candidate))
+                return true;
+        }
+        return false;
+    }
+
+    fn make(step: *std.build.Step) !void {
+        const self = @fieldParentPtr(GitRepoStep, "step", step);
+
+        std.fs.accessAbsolute(self.path, .{}) catch {
+            const branch_args = if (self.branch) |b| &[2][]const u8{ " -b ", b } else &[2][]const u8{ "", "" };
+            if (!self.fetch_enabled) {
+                std.debug.print("Error: git repository '{s}' does not exist\n", .{self.path});
+                std.debug.print("       Use -Dfetch to download it automatically, or run the following to clone it:\n", .{});
+                std.debug.print("       git clone {s}{s}{s} {s} && git -C {3s} checkout {s} -b fordep\n", .{
+                    self.url,
+                    branch_args[0],
+                    branch_args[1],
+                    self.path,
+                    self.sha,
+                });
+                std.os.exit(1);
+            }
+
+            {
+                var args = std.ArrayList([]const u8).init(self.builder.allocator);
+                defer args.deinit();
+                try args.append("git");
+                try args.append("clone");
+                try args.append(self.url);
+                try args.append(self.path);
+                if (self.branch) |branch| {
+                    try args.append("-b");
+                    try args.append(branch);
+                }
+                try run(self.builder, args.items);
+            }
+            try run(self.builder, &[_][]const u8{
+                "git",
+                "-C",
+                self.path,
+                "checkout",
+                self.sha,
+                "-b",
+                "fordep",
+            });
+        };
+
+        try self.checkSha();
+    }
+
+    fn checkSha(self: GitRepoStep) !void {
+        if (self.sha_check == .none)
+            return;
+
+        const result: union(enum) { failed: anyerror, output: []const u8 } = blk: {
+            const result = std.ChildProcess.exec(.{
+                .allocator = self.builder.allocator,
+                .argv = &[_][]const u8{
+                    "git",
+                    "-C",
+                    self.path,
+                    "rev-parse",
+                    "HEAD",
+                },
+                .cwd = self.builder.build_root,
+                .env_map = self.builder.env_map,
+            }) catch |e| break :blk .{ .failed = e };
+            try std.io.getStdErr().writer().writeAll(result.stderr);
+            switch (result.term) {
+                .Exited => |code| {
+                    if (code == 0) break :blk .{ .output = result.stdout };
+                    break :blk .{ .failed = error.GitProcessNonZeroExit };
+                },
+                .Signal => break :blk .{ .failed = error.GitProcessFailedWithSignal },
+                .Stopped => break :blk .{ .failed = error.GitProcessWasStopped },
+                .Unknown => break :blk .{ .failed = error.GitProcessFailed },
+            }
+        };
+        switch (result) {
+            .failed => |err| {
+                return self.sha_check.reportFail("failed to retreive sha for repository '{s}': {s}", .{ self.name, @errorName(err) });
+            },
+            .output => |output| {
+                if (!std.mem.eql(u8, std.mem.trimRight(u8, output, "\n\r"), self.sha)) {
+                    return self.sha_check.reportFail("repository '{s}' sha does not match\nexpected: {s}\nactual  : {s}\n", .{ self.name, self.sha, output });
+                }
+            },
+        }
+    }
+
+    fn run(builder: *std.build.Builder, argv: []const []const u8) !void {
+        {
+            var msg = std.ArrayList(u8).init(builder.allocator);
+            defer msg.deinit();
+            const writer = msg.writer();
+            var prefix: []const u8 = "";
+            for (argv) |arg| {
+                try writer.print("{s}\"{s}\"", .{ prefix, arg });
+                prefix = " ";
+            }
+            std.log.info("[RUN] {s}", .{msg.items});
+        }
+
+        var child = std.ChildProcess.init(argv, builder.allocator);
+        child.stdin_behavior = .Ignore;
+        child.stdout_behavior = .Inherit;
+        child.stderr_behavior = .Inherit;
+        child.cwd = builder.build_root;
+        child.env_map = builder.env_map;
+
+        try child.spawn();
+        const result = try child.wait();
+        switch (result) {
+            .Exited => |code| if (code != 0) {
+                std.log.err("git clone failed with exit code {}", .{code});
+                std.os.exit(0xff);
+            },
+            else => {
+                std.log.err("git clone failed with: {}", .{result});
+                std.os.exit(0xff);
+            },
+        }
+    }
+
+    pub fn getPath(self: *const GitRepoStep, who_wants_to_know: *const std.build.Step) []const u8 {
+        if (!hasDependency(who_wants_to_know, &self.step))
+            @panic("a step called GitRepoStep.getPath but has not added it as a dependency");
+        return self.path;
+    }
+};
