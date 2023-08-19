7b4c180c7cef0c1dc92d42ba3a436941cc078f29
Danh Phan <input@danh.computer>
2023-08-19 15:15:29 -0500

    tests: nan_f64 -> nan(f64)

--- tests/test.zig
+++ tests/test.zig
@@ -164,7 +164,7 @@ test "encode - float" {
         .{ "2.2250738585072014e-308", std.math.floatMin(f64) },
         .{ "1.7976931348623157e+308", std.math.floatMax(f64) },
         .{ "2.220446049250313e-16", std.math.floatEps(f64) },
-        .{ "null", std.math.nan_f64 },
+        .{ "null", std.math.nan(f64) },
         .{ "null", std.math.inf(f64) },
     };
 
