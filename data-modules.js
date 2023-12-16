22e5c29fb6e7e6af225196fe6ebe4ee07dae8d82
ibokuri <input@danh.computer>
2023-12-16 23:37:53 +0000

    Update docs to output generated at dda4272

--- data-modules.js
+++ data-modules.js
@@ -1,1 +1,1 @@
-var modules =[{"name":"json","file":0,"main":66,"table":{"getty":1,"std":2,"json":0}},{"name":"getty","file":0,"main":68,"table":{}},{"name":"std","file":0,"main":33312,"table":{}}];
+var modules =[{"name":"json","file":0,"main":66,"table":{"json":0,"getty":1,"std":2}},{"name":"getty","file":0,"main":68,"table":{}},{"name":"std","file":0,"main":33313,"table":{}}];
