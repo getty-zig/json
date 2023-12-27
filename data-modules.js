28371c3e6c6d4d67c7d9d832ebb139aab13e7f59
ibokuri <input@danh.computer>
2023-12-27 15:17:08 +0000

    Update docs to output generated at 81043be

--- data-modules.js
+++ data-modules.js
@@ -1,1 +1,1 @@
-var modules =[{"name":"json","file":0,"main":66,"table":{"json":0,"getty":1,"std":2}},{"name":"getty","file":0,"main":68,"table":{}},{"name":"std","file":0,"main":33377,"table":{}}];
+var modules =[{"name":"json","file":0,"main":66,"table":{"std":3,"json":0,"getty":1}},{"name":"getty","file":0,"main":68,"table":{"protest":2}},{"name":"protest","file":0,"main":72,"table":{}},{"name":"std","file":0,"main":33459,"table":{}}];
